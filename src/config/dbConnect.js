c5540bae5979a0b7ce9523ec3b3be437dc605015
art-Artur Ribeiro <profissional.arturrc@gmail.com>
2025-05-23 22:04:08 -0300

    livroSchema e envs

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
-import mongoose , { mongo }from "mongoose";
+import mongoose , { mongo } from "mongoose";
 
 async function ConnectDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin@cluster0.dg2dehd.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
 }
 
